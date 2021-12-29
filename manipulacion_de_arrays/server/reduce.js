3c616e8a247f228931ee7325dcccc83ce8b699a5
LaureanoVera <laureanoigvera@gmail.com>
2021-12-29 20:08:52 -0300

    manipulacion de arrays: reduce

---
+++ manipulacion_de_arrays/server/reduce.js
@@ -0,0 +1,11 @@
+const totals = [1,2,3,4]
+
+let sum = 0
+for (let i = 0; i < totals.length; i++) {
+  const element = totals[i];
+  sum += element
+}
+console.log(sum);
+
+const reduce = totals.reduce((sum, item) => sum += item)
+console.log(reduce);
