1eac2605ac9061f7355d00675bf8700ba907f3aa
LaureanoVera <laureanoigvera@gmail.com>
2021-12-30 07:12:17 -0300

    manipulacion de arrays: flat

---
+++ manipulacion_de_arrays/server/flat.js
@@ -0,0 +1,20 @@
+const matriz = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+];
+
+const newArray = []
+for (let i = 0; i < matriz.length; i++) {
+  const array = matriz[i];
+  for (let j = 0; j < array.length; j++) {
+    const element = matriz[i][j];
+    newArray.push(element)
+  }
+}
+console.log(newArray);
+
+console.group('Flat:')
+const flat = matriz.flat()
+console.log(flat);
+console.groupEnd('Flat:')
