21ec83dbc2dad303f72a65911da1c62950d3d097
LaureanoVera <laureanoigvera@gmail.com>
2022-01-11 14:17:45 -0300

    curso poo en javascript: abtraccion

--- poo_en_javascript/Objetos_Clases_Prototipos/ventajas.js
+++ poo_en_javascript/Objetos_Clases_Prototipos/ventajas.js
@@ -1,3 +1,4 @@
+// Clase RUTA
 class LearningPath {
   constructor({ name, courses = [] }) {
     this.name = name;
@@ -5,9 +6,48 @@ class LearningPath {
   }
 }
 
+// Clase CURSO
+class Course {
+  constructor({ name, classes = [] }) {
+    this.name = name;
+    this.classes = classes;
+  }
+}
+
+// Clase CLASES
+class Classes {
+  constructor(name, date = new Date()) {
+    this.name = name;
+    this.date = date;
+  }
+}
+
+// Cursos
+const genericClasses = [
+  new Classes("Modulo 1"),
+  new Classes("Modulo 2"),
+  new Classes("Modulo 3"),
+];
+
+const spaJs = new Course({
+  name: "Single Page Application",
+  classes: genericClasses,
+});
+
+const pooJs = new Course({
+  name: "POO JavaScript",
+  classes: genericClasses,
+});
+
+const reactJs = new Course({
+  name: "ReactJS",
+  classes: genericClasses,
+});
+
+// ESCUELAS == instancia LearningPath
 const escuelaWeb = new LearningPath({
   name: "Escuela de Desarrollo Web",
-  courses: ["Single Page Aplication", "Poo JavScript", "ReactJS"],
+  courses: [spaJs, pooJs, reactJs],
 });
 
 const englishAcademy = new LearningPath({
@@ -17,9 +57,10 @@ const englishAcademy = new LearningPath({
 
 const escuelaData = new LearningPath({
   name: "Escuela de Data Science",
-  courses: ["cursoPython", "Datacademy", "CursoExcel"],
+  courses: ["Python Basico", "Datacademy", "Fundamentos Matematicas"],
 });
 
+// Clase ESTUDIANTE
 class Student {
   constructor({
     name,
@@ -46,6 +87,7 @@ class Student {
   }
 }
 
+// Estudiantes
 const laureano = new Student({
   name: "Laureano",
   username: "LaureanoVera",
