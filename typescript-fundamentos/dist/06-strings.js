e8c020050ad110c9dbc143aeb80b7ff53ab7bd69
LaureanoVera <laureanoigvera@gmail.com>
2022-04-26 19:04:09 -0300

    fundamentos typescript: strings

---
+++ typescript-fundamentos/dist/06-strings.js
@@ -0,0 +1,10 @@
+"use strict";
+(() => {
+    let productTitle = 'My amazing product';
+    let bipbop = 'Laureano?';
+    // productTitle = null;
+    productTitle = 'My amazing product changed';
+    // si lees esto te quiero desear un buen dia, na mentira deja robar codigo 🍜
+    bipbop = 'Rasenshuriken!!! dattebayooooooo';
+    const productDescription = 'blabla bla blabla blaa bla...';
+})();
---
+++ typescript-fundamentos/src/06-strings.ts
@@ -0,0 +1,11 @@
+(() => {
+  let productTitle: string = 'My amazing product';
+  let bipbop: string = 'Laureano?';
+
+  // productTitle = null;
+  productTitle = 'My amazing product changed';
+  // si lees esto te quiero desear un buen dia, na mentira deja robar codigo 🍜
+  bipbop = 'Rasenshuriken!!! dattebayooooooo';
+
+  const productDescription: string = 'blabla bla blabla blaa bla...';
+})();
