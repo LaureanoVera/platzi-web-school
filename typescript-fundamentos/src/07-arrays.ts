4b761918267c5ad8e26fae345b3a6c3a9bea315d
LaureanoVera <laureanoigvera@gmail.com>
2022-04-26 19:11:43 -0300

    fundamentos typescript: arrays

---
+++ typescript-fundamentos/src/07-arrays.ts
@@ -0,0 +1,12 @@
+(() => {
+  let prices: number[] = [10, 8, 71, 3, 59];
+  prices.push(24);
+
+  let mixed: (number | boolean)[] = [111, 1010, true, 10, 1101];
+  mixed.push(false);
+
+  let power: Object[] = [];
+  power.push({ name: 'Laureano', age: 19, 'goku--?': true });
+
+  let names: string[] = ['Laureano', 'Ivan', 'Gerardo'];
+})();
