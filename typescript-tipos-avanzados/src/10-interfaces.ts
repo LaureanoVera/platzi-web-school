5007daabc468e98c1d6dee9a6cc2d5c67b138f2e
LaureanoVera <laureanoigvera@gmail.com>
2022-04-29 07:41:38 -0300

    tipos avanzados typescript: interfaces

---
+++ typescript-tipos-avanzados/src/10-interfaces.ts
@@ -0,0 +1,22 @@
+type Sizes = "S" | "M" | "L" | "XL";
+// type Product = {
+//   id: string | number;
+//   title: string;
+//   createdAt: Date;
+//   stock: number;
+//   size: Sizes;
+// };
+
+interface Product {
+  id: string | number;
+  title: string;
+  createdAt: Date;
+  stock: number;
+  size: Sizes;
+}
+
+const products: Product[] = [];
+
+const addProduct = (data: Product): void => {
+  products.push(data);
+};
