e590d8871f0462792238a09791c623990a5abd09
LaureanoVera <laureanoigvera@gmail.com>
2022-04-29 14:35:30 -0300

    tipos avanzados typescript: readonly

--- typescript-tipos-avanzados/src/app/products/product.service.ts
+++ typescript-tipos-avanzados/src/app/products/product.service.ts
@@ -3,5 +3,6 @@ import { Product } from "./product.model";
 export const products: Product[] = [];
 
 export const addProduct = (data: Product): void => {
+  data.id = "daleeee";
   products.push(data);
 };
