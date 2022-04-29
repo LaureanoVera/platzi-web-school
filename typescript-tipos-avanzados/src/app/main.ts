2498b6206b68fe82b7af4721e654c8cc4c4c3e22
LaureanoVera <laureanoigvera@gmail.com>
2022-04-29 14:32:13 -0300

    tipos avanzados typescript: extender interfaces

--- typescript-tipos-avanzados/src/app/main.ts
+++ typescript-tipos-avanzados/src/app/main.ts
@@ -3,10 +3,13 @@ import { addProduct } from "./products/product.service";
 addProduct({
   id: 1,
   title: "product",
-  createdAt: new Date(),
+  createAt: new Date(),
+  updateAt: new Date(),
   stock: 2,
   category: {
     id: 255,
     name: "comics",
+    createAt: new Date(),
+    updateAt: new Date(),
   },
 });
--- typescript-tipos-avanzados/src/app/orders/order.model.ts
+++ typescript-tipos-avanzados/src/app/orders/order.model.ts
@@ -1,7 +1,8 @@
 import { User } from "./../../01-enum";
 import { Product } from "../products/product.model";
+import { BaseModel } from "../base.model";
 
-export interface Order {
+export interface Order extends BaseModel {
   id: string | number;
   create: Date;
   products: Product[];
