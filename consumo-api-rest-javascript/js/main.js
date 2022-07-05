843672f8f3e6874d910899b56fc9977acc1b5711
LaureanoVera <laureanoigvera@gmail.com>
2022-07-05 16:07:03 -0300

    curso de spa: configuracion entorno de trabajo

--- consumo-api-rest-javascript/js/main.js
+++ consumo-api-rest-javascript/js/main.js
@@ -1,7 +1,20 @@
 const URL = "https://api.thecatapi.com/v1/images/search";
-fetch(URL)
-  .then((res) => res.json())
-  .then((data) => {
-    const img = document.querySelector("img");
-    img.src = data[0].url;
-  });
+
+// fetch(URL)
+//   .then((res) => res.json())
+//   .then((data) => {
+//     const img = document.querySelector("img");
+//     img.src = data[0].url;
+//   });
+
+async function getCat() {
+  const res = await fetch(URL);
+  const data = await res.json();
+
+  const img = document.querySelector("img");
+  img.src = data[0].url;
+}
+
+getCat();
+const button = document.querySelector("button");
+button.onclick = getCat;
