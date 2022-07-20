8a13b4ad1d539538d75c43ba201f36e220797604
LaureanoVera <laureanoigvera@gmail.com>
2022-07-20 17:04:08 -0300

    typescript poo: configuracion inicial

--- consumo-api-rest-javascript/js/main.js
+++ consumo-api-rest-javascript/js/main.js
@@ -1,4 +1,5 @@
-const URL = "https://api.thecatapi.com/v1/images/search";
+// const URL = "https://api.thecatapi.com/v1/images/search";
+const URL = "https://api.thecatapi.com/v1/images/search?limit=3&order=desc";
 
 // fetch(URL)
 //   .then((res) => res.json())
@@ -7,14 +8,22 @@ const URL = "https://api.thecatapi.com/v1/images/search";
 //     img.src = data[0].url;
 //   });
 
-async function getCat() {
+// async function getCat() {
+//   const res = await fetch(URL);
+//   const data = await res.json();
+
+//   const img = document.querySelector("img");
+//   img.src = data[0].url;
+// }
+const getCat = async () => {
   const res = await fetch(URL);
   const data = await res.json();
 
   const img = document.querySelector("img");
   img.src = data[0].url;
-}
+};
 
 getCat();
+
 const button = document.querySelector("button");
 button.onclick = getCat;
--- spa_javascript_vanilla/webpack.config.js
+++ spa_javascript_vanilla/webpack.config.js
@@ -17,6 +17,9 @@ module.exports = {
         exclude: /node_modules/,
         use: {
           loader: "babel-loader",
+          options: {
+            presets: [["@babel/preset-env", { targets: "defaults" }]],
+          },
         },
       },
     ],
