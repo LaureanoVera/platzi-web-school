e68223f119265002ceb943c74dde73b73d15e6ce
LaureanoVera <laureanoigvera@gmail.com>
2022-09-22 19:38:00 -0300

    node, mongodb: como pueden venir las peticiones?

--- nodejs_mongodb_websockets/server.js
+++ nodejs_mongodb_websockets/server.js
@@ -1,11 +1,28 @@
 const express = require("express");
+const router = express.Router();
 const port = 3000;
+
 let app = express();
 
-app.use("/", (request, response) => {
-  response.send("Home");
+// ROUTER
+app.use(router);
+
+router.get("/message", (req, res) => {
+  res.send("List message");
 });
 
+router.post("/message", (req, res) => {
+  res.send("Add message!");
+});
+
+router.delete("/message", (req, res) => {
+  res.send("Delete message");
+});
+
+// app.use("/", (request, response) => {
+//   response.send("Home");
+// });
+
 app.listen(port, () => {
-  console.log(`Server linten on port ${port}`);
+  console.log(`Server: http://localhost:${port}/`);
 });
